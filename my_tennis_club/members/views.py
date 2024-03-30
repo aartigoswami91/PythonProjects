840b68b2669a546c4186ebe1f0d174cc355ccbb9
Aarti Goswami <goswamiaarti7272@gmail.com>
2024-03-29 21:52:00 -0400

    edit

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import UserForm
 from .models import User
 from django.http import HttpResponse
@@ -19,6 +19,24 @@ def user_list(request):
     users = User.objects.all()
     return render(request, 'user_list.html', {'users': users})
 
+
+def edit_user(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user_list')
+    else:
+        form = UserForm(instance=user)
+    return render(request, 'edit_user.html', {'form': form, 'user': user})
+
+def delete_user(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    if request.method == 'POST':
+        user.delete()
+        return redirect('user_list')
+    return render(request, 'delete_user.html', {'user': user})
 # Create your views here.
 
 def members(request):
--- my_tennis_club/my_tennis_club/urls.py
+++ my_tennis_club/my_tennis_club/urls.py
@@ -23,5 +23,7 @@ urlpatterns = [
     path('members/', views.members, name='members'),
     path('register/', views.register, name='register'),
     path('user-list/', views.user_list, name='user_list'),
+    path('edit-user/<int:user_id>/', views.edit_user, name='edit_user'),
+    path('delete-user/<int:user_id>/', views.delete_user, name='delete_user'),
 ]
 
